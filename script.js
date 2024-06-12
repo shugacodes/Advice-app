941095278aad2bca4ce179dca82459c9655098a1
shugacodes <shugaglow@gmail.com>
2024-06-12 19:39:20 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const adviceEl = document.querySelector(".advice");
+const btnEl = document.querySelector(".btn");
+const countEl = document.querySelector(".count");
+
+const getAdvice = async function () {
+const res = await fetch("https://api.adviceslip.com/advice");
+const data = await res.json();
+
+// Updating values
+advice = data.slip.advice;
+count = count + 1;
+
+// Manually updating DOM elements
+countEl.textContent = count;
+adviceEl.textContent = advice;
+};
+
+// Setting initial values
+let count = 0;
+let advice;
+getAdvice();
+
+// Attaching an event listener
+btnEl.addEventListener("click", getAdvice);
